7ba59aeed586d2ee8e3b27977c63e9ff96b706dd
lvyufeng <lvyufeng@cqu.edu.cn>
2018-07-28 21:35:28 +0800

    add axi_interface

---
+++ rtl/axi_interface.v
@@ -0,0 +1,92 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2018/07/28 20:15:33
+// Design Name: 
+// Module Name: axi_interface
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module axi_interface(
+    input wire clk,
+    input wire resetn,
+    
+    //inst sram-like
+    input wire inst_req,
+    input wire inst_wr,
+    input wire[1:0] inst_size,
+    input wire[31:0] inst_addr,
+    input wire[31:0] inst_wdata,
+    output wire[31:0] inst_rdata,
+    output wire inst_addr_ok,
+    output wire inst_data_ok,
+
+    // data sram-like
+    input wire data_req,
+    input wire data_wr,
+    input wire[1:0] data_size,
+    input wire[3:0] data_wen,
+    input wire[31:0] data_addr,
+    input wire[31:0] data_wdata,
+    output wire[31:0] data_rdata,
+    output wire data_addr_ok,
+    output wire data_data_ok,
+
+    // axi port
+    //ar
+    output wire[3:0] arid,      //read request id, fixed 4'b0
+    output wire[31:0] araddr,   //read request address
+    output wire[3:0] arlen,     //read request transfer length(beats), fixed 4'b0
+    output wire[2:0] arsize,    //read request transfer size(bytes per beats)
+    output wire[1:0] arburst,   //transfer type, fixed 2'b01
+    output wire[1:0] arlock,    //atomic lock, fixed 2'b0
+    output wire[3:0] arcache,   //cache property, fixed 4'b0
+    output wire[2:0] arprot,    //protect property, fixed 3'b0
+    output wire arvalid,        //read request address valid
+    input wire arready,         //slave end ready to receive address transfer
+    //r              
+    input wire[3:0] rid,        //equal to arid, can be ignored
+    input wire[31:0] rdata,     //read data
+    input wire[1:0] rresp,      //this read request finished successfully, can be ignored
+    input wire rlast,           //the last beat data for this request, can be ignored
+    input wire rvalid,          //read data valid
+    output wire rready,         //master end ready to receive data transfer
+    //aw           
+    output wire[3:0] awid,      //write request id, fixed 4'b0
+    output wire[31:0] awaddr,   //write request address
+    output wire[3:0] awlen,     //write request transfer length(beats), fixed 4'b0
+    output wire[2:0] awsize,    //write request transfer size(bytes per beats)
+    output wire[1:0] awburst,   //transfer type, fixed 2'b01
+    output wire[1:0] awlock,    //atomic lock, fixed 2'b01
+    output wire[3:0] awcache,   //cache property, fixed 4'b01
+    output wire[2:0] awprot,    //protect property, fixed 3'b01
+    output wire awvalid,        //write request address valid
+    input wire awready,         //slave end ready to receive address transfer
+    //w          
+    output wire[3:0] wid,       //equal to awid, fixed 4'b0
+    output wire[31:0] wdata,    //write data
+    output wire[3:0] wstrb,     //write data strobe select bit
+    output wire wlast,          //the last beat data signal, fixed 1'b1
+    output wire wvalid,         //write data valid
+    input wire wready,          //slave end ready to receive data transfer
+    //b              
+    input  wire[3:0] bid,       //equal to wid,awid, can be ignored
+    input  wire[1:0] bresp,     //this write request finished successfully, can be ignored
+    input wire bvalid,          //write data valid
+    output wire bready          //master end ready to receive write response
+
+    );
+endmodule
